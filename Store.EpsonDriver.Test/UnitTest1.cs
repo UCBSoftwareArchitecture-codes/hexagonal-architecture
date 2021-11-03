73c2d6c852f973fb482f3c2eac633de06e306130
Pablo Sangueza <pablo.sangueza@niceincontact.com>
2021-11-03 11:08:58 -0400

    add solution and unit test components

---
+++ Store.EpsonDriver.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Store.EpsonDriver.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- Store.Web/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Store.Web
-{
-    public class Class1
-    {
-    }
-}
